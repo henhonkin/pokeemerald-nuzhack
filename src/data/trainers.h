c251cb9fddc8184238d1cb15c34c19c89b047efc
Tinend <ulrich.brodowsky@gmx.net>
2024-02-18 17:11:42 +0100

    4. Trainer

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -7254,7 +7254,7 @@ const struct Trainer gTrainers[] = {
         .trainerName = _("TIANA"),
         .items = {},
         .doubleBattle = FALSE,
-        .aiFlags = AI_FLAG_CHECK_BAD_MOVE,
+        .aiFlags = AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_TRY_TO_FAINT | AI_FLAG_CHECK_VIABILITY,
         .party = TRAINER_PARTY(sParty_Tiana),
     },
 
