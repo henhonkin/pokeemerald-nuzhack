a6d3613e52ee7be90d25057146233a278b98cc7c
Tinend <ulrich.brodowsky@gmx.net>
2024-02-25 11:31:45 +0100

    Trainer Route 102 überarbeitet, erster Trainer Route 104

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -3836,7 +3836,7 @@ const struct Trainer gTrainers[] = {
         .trainerName = _("BILLY"),
         .items = {},
         .doubleBattle = FALSE,
-        .aiFlags = AI_FLAG_CHECK_BAD_MOVE,
+        .aiFlags = AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_TRY_TO_FAINT | AI_FLAG_CHECK_VIABILITY,
         .party = TRAINER_PARTY(sParty_Billy),
     },
 
